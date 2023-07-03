723f07085fe4cf2756341f83fb4ae359912ce031
Axel Malka <axelmalka@outlook.fr>
2023-07-03 15:21:57 +0000

    fix type issues w datetime

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""Import uuid and datetime modules"""
 import uuid
 import datetime
 """Base models"""
-
 
 class BaseModel:
     """base class"""
 
     def __init__(self):
-        """Initialize instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+        """Initialize instance"""     
+        self.id = str(uuid.uuid4())   
         self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.datetime.now()
 
     def __str__(self):
         """return the string representation"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.to_dict()}"
 
     def save(self):
         """Update the date variable"""
