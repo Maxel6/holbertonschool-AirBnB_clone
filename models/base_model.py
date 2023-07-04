e34c73735360cbd6931f7df89ff03fc55a1d3885
Irrisam <tristanduchamp@hotmail.fr>
2023-07-04 07:39:14 +0200

    Test a solution altough main little different

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,19 @@ import datetime
 class BaseModel:
     """base class"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Initialize instance"""
-        self.id = str(uuid.uuid4())
-        self.updated_at = datetime.datetime.now()
-        self.created_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.datetime.strptime(value,
+                                                       "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.datetime.now()
 
     def __str__(self):
         """return the string representation"""
@@ -30,3 +38,26 @@ class BaseModel:
         self.__dict__["__class__"] = self.__class__.__name__
 
         return self.__dict__
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
