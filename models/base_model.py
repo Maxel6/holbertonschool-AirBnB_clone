00b9a2c8e12b408ed50e29da8ea74ffd5c654a8d
Irrisam <tristanduchamp@hotmail.fr>
2023-07-04 07:53:49 +0200

    WIP kwargs values, still error on id build

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,14 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.datetime.strptime(value,
-                                                       "%Y-%m-%dT%H:%M:%S.%f")
-                if key != '__class__':
-                    setattr(self, key, value)
+                    value = datetime.datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+            if key != '__class__':
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.updated_at = datetime.datetime.now()
             self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """return the string representation"""
