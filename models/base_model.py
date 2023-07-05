49b85047d015aefe52664e6c6c2d5f8ad8ec848b
Irrisam <tristanduchamp@hotmail.fr>
2023-07-05 07:12:59 +0200

    update to dict func for a copy of current instance

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize instance"""
-        if kwargs:
+        if kwargs is True:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.datetime.strptime(
@@ -30,34 +30,15 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """update and return __dict__"""
-        if isinstance(self.created_at, datetime.datetime):
-            self.__dict__['created_at'] = self.created_at.isoformat()
-        if isinstance(self.updated_at, datetime.datetime):
-            self.__dict__['updated_at'] = self.updated_at.isoformat()
-        self.__dict__["__class__"] = self.__class__.__name__
+        """turns instance into dict
 
-        return self.__dict__
-
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
+        Returns:
+            dict: dict form of instance
+        """
+        copy_dict = self.__dict__.copy()      
+        copy_dict['__class__'] = type(self).__name__
+        if not isinstance(copy_dict['created_at'], str):
+            copy_dict['created_at'] = self.created_at.isoformat()
+        if not isinstance(copy_dict['updated_at'], str):
+            copy_dict['updated_at'] = self.updated_at.isoformat()
+        return (copy_dict)
